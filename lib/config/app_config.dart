9d0ca8eea58bfe172e3815a6355ab5db58c7d8bf
mahmoudhan2025-o <mahmoud.han2025@gmail.com>
2026-03-31 19:15:20 +0200

    feat: Add product management features with data fetching and display
    - Created AppConfig class to store API URLs for products and reviews.
    - Implemented Product model to represent product data.
    - Developed DataService to fetch product data from a CSV file and handle errors.
    - Added product providers using Riverpod for state management, including data fetching and search filters.
    - Built HomePage screen to display products with search, filtering, and sorting capabilities.
    - Designed ProductCard and ProductDetailsPage for individual product display and review submission.
    - Integrated shimmer effect for loading states and improved UI responsiveness.

---
+++ lib/config/app_config.dart
@@ -0,0 +1,6 @@
+class AppConfig {
+  static const String productsCsvUrl =
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vR7MlbyiyWcpPPqdzFyF8yOP21xKMK6emc63s_6peyXIXSHWG6H-G9MrST7dD9oyqXgA5MfeHM70XG3/pub?output=csv";
+  static const String reviewsApiUrl =
+      "https://script.google.com/macros/s/AKfycbyUhaXAYEWKmLbCuzRCMgrfTPjDUuEt_O2lJeaWPMBG_zIw0MUl5VfwFYhoDBSBpsnDgA/exec";
+}
