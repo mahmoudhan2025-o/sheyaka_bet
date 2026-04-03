6ddfac50af60c48f8089cd5732439f215b4af833
mahmoudhan2025-o <mahmoud.han2025@gmail.com>
2026-04-03 15:46:34 +0200

    إطلاق نسخة الويب النهائية المستقرة

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "40bc5d85011826b28f6b60044fa1eb39",
+"flutter_bootstrap.js": "84c941953c115ddcfc42a7303d11883b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
